21f74b49158d1990beeefff481482efbf8b0a3ed
Ryoichi Hara <rychhr@gmail.com>
2014-06-28 21:50:48 +0900

    Update Array2D

--- CookieCrunch-Swift/Array2D.swift
+++ CookieCrunch-Swift/Array2D.swift
@@ -7,3 +7,24 @@
 //
 
 import Foundation
+
+class Array2D<T> {
+    let columns: Int
+    let rows: Int
+    let array: Array<T?>  // private
+
+    init(columns: Int, rows: Int) {
+        self.columns = columns
+        self.rows = rows
+        array = Array<T?>(count: rows * columns, repeatedValue: nil)
+    }
+
+    subscript(column: Int, row: Int) -> T? {
+        get {
+            return array[row * columns + column]
+        }
+        set {
+            array[row * columns + column] = newValue
+        }
+    }
+}
