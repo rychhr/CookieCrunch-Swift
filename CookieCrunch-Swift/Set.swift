988049c815fa895cd44dab97071e408a2f6c5f42
Ryoichi Hara <rychhr@gmail.com>
2014-06-28 22:22:06 +0900

    Update Set.swift

--- CookieCrunch-Swift/Set.swift
+++ CookieCrunch-Swift/Set.swift
@@ -6,4 +6,49 @@
 //  Copyright (c) 2014 Ryoichi Hara. All rights reserved.
 //
 
-import Foundation
+class Set<T: Hashable>: Sequence, Printable {
+    var dictionary = Dictionary<T, Bool>()  // private
+
+    func addElement(newElement: T) {
+        dictionary[newElement] = true
+    }
+
+    func removeElement(element: T) {
+        dictionary[element] = nil
+    }
+
+    func containsElement(element: T) -> Bool {
+        return dictionary[element] != nil
+    }
+
+    func allElements() -> T[] {
+        return Array(dictionary.keys)
+    }
+
+    var count: Int {
+        return dictionary.count
+    }
+
+    func unionSet(otherSet: Set<T>) -> Set<T> {
+        var combined = Set<T>()
+
+        for obj in dictionary.keys {
+            combined.dictionary[obj] = true
+        }
+
+        for obj in otherSet.dictionary.keys {
+            combined.dictionary[obj] = true
+        }
+
+        return combined
+    }
+
+    // Allows you to use the set in 'for-in' loops
+    func generate() -> IndexingGenerator<Array<T>> {
+        return allElements().generate()
+    }
+
+    var description: String {
+        return dictionary.description
+    }
+}
