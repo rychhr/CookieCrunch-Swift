d123faf1faa7c602e9fb473159005164a2c152dc
Ryoichi Hara <rychhr@gmail.com>
2014-06-28 22:49:14 +0900

    Update Level

--- CookieCrunch-Swift/Level.swift
+++ CookieCrunch-Swift/Level.swift
@@ -7,3 +7,38 @@
 //
 
 import Foundation
+
+let NumColumns = 9
+let NumRows    = 9
+
+class Level {
+    let cookies = Array2D<Cookie>(columns: NumColumns, rows: NumRows)  // private
+
+    func cookieAtColumn(column: Int, row: Int) -> Cookie? {
+        assert(column >= 0 && column < NumColumns)
+        assert(row >= 0 && row < NumRows)
+        return cookies[column, row]
+    }
+
+    func shuffle() -> Set<Cookie> {
+        return createInitialCookies()
+    }
+
+    func createInitialCookies() -> Set<Cookie> {
+        var set = Set<Cookie>()
+
+        for row in 0..NumRows {
+            for column in 0..NumColumns {
+
+                var cookieType = CookieType.random()
+
+                let cookie = Cookie(column: column, row: row, cookieType: cookieType)
+                cookies[column, row] = cookie
+
+                set.addElement(cookie)
+            }
+        }
+
+        return set
+    }
+}
