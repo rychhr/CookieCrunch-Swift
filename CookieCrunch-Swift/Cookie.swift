23c1fbc1294e97d6aca7cbdc1bc2fad91b5cc82c
Ryoichi Hara <rychhr@gmail.com>
2014-06-28 21:21:43 +0900

    Update Cookie

--- CookieCrunch-Swift/Cookie.swift
+++ CookieCrunch-Swift/Cookie.swift
@@ -6,4 +6,49 @@
 //  Copyright (c) 2014 Ryoichi Hara. All rights reserved.
 //
 
-import Foundation
+import SpriteKit
+
+enum CookieType: Int {
+    case Unknown = 0, Croissant, Cupcake, Danish, Donut, Macaroon, SugarCookie
+
+    var spriteName: String {
+        let spriteNames = [
+            "Croissant",
+            "Cupcake",
+            "Danish",
+            "Donut",
+            "Macaroon",
+            "SugarCookie"]
+
+        return spriteNames[toRaw() - 1]
+    }
+
+    var highlightedSpriteName: String {
+        let highlightedSpriteNames = [
+            "Croissant-Highlighted",
+            "Cupcake-Highlighted",
+            "Danish-Highlighted",
+            "Donut-Highlighted",
+            "Macaroon-Highlighted",
+            "SugarCookie-Highlighted"]
+
+        return highlightedSpriteNames[toRaw() - 1]
+    }
+
+    static func random() -> CookieType {
+        return CookieType.fromRaw(Int(arc4random_uniform(6)) + 1)!
+    }
+}
+
+class Cookie {
+    var column: Int
+    var row: Int
+    let cookieType: CookieType
+    var sprite: SKSpriteNode?
+
+    init(column: Int, row: Int, cookieType: CookieType) {
+        self.column = column
+        self.row = row
+        self.cookieType = cookieType
+    }
+}
