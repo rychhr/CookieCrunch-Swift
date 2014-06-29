2194f595c2c178538c525b81b9579e366a6e290f
Ryoichi Hara <rychhr@gmail.com>
2014-06-29 12:03:12 +0900

    Load levels from json files

--- CookieCrunch-Swift/Level.swift
+++ CookieCrunch-Swift/Level.swift
@@ -15,6 +15,26 @@ class Level {
     let cookies = Array2D<Cookie>(columns: NumColumns, rows: NumRows)  // private
     let tiles = Array2D<Tile>(columns: NumColumns, rows: NumRows)      // private
 
+    init(filename: String) {
+        if let dictionary = Dictionary<String, AnyObject>.loadJSONFromBundle(filename) {
+
+            if let tilesArray: AnyObject = dictionary["tiles"] {
+
+                // Int[][]: array of array of Int
+                for (row, rowArray) in enumerate(tilesArray as Int[][]) {
+                    let tileRow = NumRows - row - 1
+
+                    // enumerate: returns the current row number and value
+                    for (column, value) in enumerate(rowArray) {
+                        if value == 1 {
+                            tiles[column, tileRow] = Tile()
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     func cookieAtColumn(column: Int, row: Int) -> Cookie? {
         assert(column >= 0 && column < NumColumns)
         assert(row >= 0 && row < NumRows)
@@ -31,12 +51,14 @@ class Level {
         for row in 0..NumRows {
             for column in 0..NumColumns {
 
-                var cookieType = CookieType.random()
+                if tiles[column, row] != nil {
+                    var cookieType = CookieType.random()
 
-                let cookie = Cookie(column: column, row: row, cookieType: cookieType)
-                cookies[column, row] = cookie
+                    let cookie = Cookie(column: column, row: row, cookieType: cookieType)
+                    cookies[column, row] = cookie
 
-                set.addElement(cookie)
+                    set.addElement(cookie)
+                }
             }
         }
 
