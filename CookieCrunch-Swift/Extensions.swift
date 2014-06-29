d290b4b330ab0082afbab71f907c154016ff6dca
Ryoichi Hara <rychhr@gmail.com>
2014-06-29 11:44:42 +0900

    Update Extensions

--- CookieCrunch-Swift/Extensions.swift
+++ CookieCrunch-Swift/Extensions.swift
@@ -7,3 +7,29 @@
 //
 
 import Foundation
+
+extension Dictionary {
+    static func loadJSONFromBundle(filename: String) -> Dictionary<String, AnyObject>? {
+        let path = NSBundle.mainBundle().pathForResource(filename, ofType: "json")
+        if !path {
+            println("Could not find level file: \(filename)")
+            return nil
+        }
+
+        var error: NSError?
+        let data: NSData? = NSData(contentsOfFile: path, options: NSDataReadingOptions(), error: &error)
+        if !data {
+            println("Could not find level file: \(filename), error: \(error!)")
+            return nil
+        }
+
+        let dictionary: AnyObject! = NSJSONSerialization
+            .JSONObjectWithData(data, options: NSJSONReadingOptions(), error: &error)
+        if !dictionary {
+            println("Level file '\(filename)' is not valid JSON: \(error!)")
+            return nil
+        }
+
+        return dictionary as? Dictionary<String, AnyObject>
+    }
+}
